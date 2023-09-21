784ba91863613453df3310fcb42a58ba21c34562
0xlax <harshu8310@gmail.com>
2023-09-21 21:23:46 +0530

    add formatOp function

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -122,6 +122,13 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
   maximumFractionDigits: 0,
 })
 
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
 
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
